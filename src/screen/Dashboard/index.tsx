95f61dd3283be222ed1cdd900e99c75f75341a2a
Schitzos <rizky.rachmawan25@gmail.com>
2024-01-11 19:09:06 +0700

    fix: sample routing navigation

--- src/screen/Dashboard/index.tsx
+++ src/screen/Dashboard/index.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import TextView from '@components/TextView';
-import {View, StyleSheet, SafeAreaView} from 'react-native';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {RootStackParamList} from 'src/navigation/types';
+import SafeArea from '@components/SafeArea';
+import Button from '@components/Button';
+import Content from '@components/Content';
+import theme from '@theme/index';
 
-export default function Dashboard() {
+type DashboardScreenProps = {
+  route: any;
+  navigation: StackNavigationProp<RootStackParamList, 'Dashboard'>;
+};
+
+export default function Dashboard({navigation}: DashboardScreenProps) {
   return (
-    <SafeAreaView style={styles.base}>
-      <View>
-        <TextView>THIS IS DASHBOARD asdasd</TextView>
-      </View>
-    </SafeAreaView>
+    <SafeArea>
+      <Content color={theme.colors.neutral50}>
+        <TextView>This is Dashboard</TextView>
+        <Button onPress={() => navigation.navigate('PokemonDetail')} size="md">
+          Detail
+        </Button>
+      </Content>
+    </SafeArea>
   );
 }
-
-const styles = StyleSheet.create({
-  base: {
-    backgroundColor: '#333333',
-  },
-  text: {
-    color: 'red',
-  },
-});
