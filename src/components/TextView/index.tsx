d5116caaa2cf727c3cbb1982eb114c22bedb749c
Schitzos <rizky.rachmawan25@gmail.com>
2024-01-13 17:04:07 +0700

    fix(component): adjust capitalize

--- src/components/TextView/index.tsx
+++ src/components/TextView/index.tsx
@@ -7,6 +7,7 @@ interface TextViewProps {
   fz?: number;
   fw?: '400' | '600' | '700' | 'bold' | 'normal';
   align?: 'center' | 'auto' | 'left' | 'right' | 'justify' | undefined;
+  capitalize?: boolean;
 }
 
 export default function TextView({
@@ -15,6 +16,7 @@ export default function TextView({
   fz = 12,
   fw = 'normal',
   align = 'left',
+  capitalize = true,
 }: TextViewProps) {
   const styles = StyleSheet.create({
     base: {
@@ -22,6 +24,7 @@ export default function TextView({
       fontSize: fz,
       fontWeight: fw,
       textAlign: align,
+      textTransform: capitalize ? 'capitalize' : 'none',
     },
   });
 
