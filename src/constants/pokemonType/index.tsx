86acaa3c4002c7e99e29eed82acbe54e596e7022
Schitzos <rizky.rachmawan25@gmail.com>
2024-01-13 17:04:41 +0700

    feat(constant): add mapping type pokemon type assets

---
+++ src/constants/pokemonType/index.tsx
@@ -0,0 +1,102 @@
+import IconNormal from '@assets/icons/icon-normal.svg';
+import IconFighting from '@assets/icons/icon-fighting.svg';
+import IconFlying from '@assets/icons/icon-flying.svg';
+import IconPoison from '@assets/icons/icon-poison.svg';
+import IconGround from '@assets/icons/icon-ground.svg';
+import IconRock from '@assets/icons/icon-rock.svg';
+import IconBug from '@assets/icons/icon-bug.svg';
+import IconGhost from '@assets/icons/icon-ghost.svg';
+import IconSteel from '@assets/icons/icon-steel.svg';
+import IconFire from '@assets/icons/icon-fire.svg';
+import IconWater from '@assets/icons/icon-water.svg';
+import IconGrass from '@assets/icons/icon-grass.svg';
+import IconElectric from '@assets/icons/icon-electric.svg';
+import IconPsychic from '@assets/icons/icon-psychic.svg';
+import IconIce from '@assets/icons/icon-ice.svg';
+import IconDragon from '@assets/icons/icon-dragon.svg';
+import IconDark from '@assets/icons/icon-dark.svg';
+import IconFairy from '@assets/icons/icon-fairy.svg';
+import IconShadow from '@assets/icons/icon-shadow.svg';
+
+export const pokemonType = [
+  {
+    name: 'normal',
+    icon: IconNormal,
+  },
+  {
+    name: 'fighting',
+    icon: IconFighting,
+  },
+  {
+    name: 'flying',
+    icon: IconFlying,
+  },
+  {
+    name: 'poison',
+    icon: IconPoison,
+  },
+  {
+    name: 'ground',
+    icon: IconGround,
+  },
+  {
+    name: 'rock',
+    icon: IconRock,
+  },
+  {
+    name: 'bug',
+    icon: IconBug,
+  },
+  {
+    name: 'ghost',
+    icon: IconGhost,
+  },
+  {
+    name: 'steel',
+    icon: IconSteel,
+  },
+  {
+    name: 'fire',
+    icon: IconFire,
+  },
+  {
+    name: 'water',
+    icon: IconWater,
+  },
+  {
+    name: 'grass',
+    icon: IconGrass,
+  },
+  {
+    name: 'electric',
+    icon: IconElectric,
+  },
+  {
+    name: 'psychic',
+    icon: IconPsychic,
+  },
+  {
+    name: 'ice',
+    icon: IconIce,
+  },
+  {
+    name: 'dragon',
+    icon: IconDragon,
+  },
+  {
+    name: 'dark',
+    icon: IconDark,
+  },
+  {
+    name: 'fairy',
+    icon: IconFairy,
+  },
+  {
+    name: 'unknown',
+    icon: IconNormal,
+  },
+  {
+    name: 'shadow',
+    icon: IconShadow,
+  },
+];
