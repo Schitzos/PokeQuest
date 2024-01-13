c01d726a283453bd4829f2e32929f24f73e06961
Schitzos <rizky.rachmawan25@gmail.com>
2024-01-13 17:03:04 +0700

    fix(app): handle splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import * as React from 'react';
+import React, {useEffect} from 'react';
 import Navigation from './navigation';
+import SplashScreen from 'react-native-splash-screen';
 import {QueryClientProvider} from '@tanstack/react-query';
 import {queryClientConfig} from './config/tanstack';
 
 const queryClient = queryClientConfig;
 
 function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <Navigation />
