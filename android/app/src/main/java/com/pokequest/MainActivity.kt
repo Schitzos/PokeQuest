ef60b9dd4a844b847a9cafb7937cd3b068827b52
Schitzos <rizky.rachmawan25@gmail.com>
2024-01-13 17:09:34 +0700

    feat(icon): add app icon and splash screen

--- android/app/src/main/java/com/pokequest/MainActivity.kt
+++ android/app/src/main/java/com/pokequest/MainActivity.kt
@@ -4,6 +4,7 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import org.devio.rn.splashscreen.SplashScreen;
 import android.os.Bundle;
 
 class MainActivity : ReactActivity() {
@@ -12,7 +13,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "pokeQuest"
+  override fun getMainComponentName(): String = "PokeQuest"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
@@ -22,6 +23,7 @@ class MainActivity : ReactActivity() {
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 
    override fun onCreate(savedInstanceState: Bundle?) {
+    SplashScreen.show(this)
     super.onCreate(null)
   }
 }
--- ios/pokeQuest/AppDelegate.mm
+++ ios/pokeQuest/AppDelegate.mm
@@ -1,4 +1,5 @@
 #import "AppDelegate.h"
+#import "RNSplashScreen.h"
 
 #import <React/RCTBundleURLProvider.h>
 
@@ -6,12 +7,13 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"pokeQuest";
+  self.moduleName = @"PokeQuest";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+  bool didFinish = [super application:application didFinishLaunchingWithOptions:launchOptions];
+  [RNSplashScreen show];  // this needs to be called after [super application:application didFinishLaunchingWithOptions:launchOptions];
+  return didFinish;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
