756de5c22914bde561a2aeaf8a827fa9434d0a5c
Schitzos <rizky.rachmawan25@gmail.com>
2024-01-22 21:25:36 +0700

    feat: add constant pokemon color

---
+++ src/constants/pokemonColor/index.tsx
@@ -0,0 +1,42 @@
+export const pokemonColor = [
+  {
+    name: 'black',
+    background: require('@assets/images/black-background.png'),
+  },
+  {
+    name: 'blue',
+    background: require('@assets/images/blue-background.png'),
+  },
+  {
+    name: 'brown',
+    background: require('@assets/images/brown-background.png'),
+  },
+  {
+    name: 'gray',
+    background: require('@assets/images/white-background.png'),
+  },
+  {
+    name: 'green',
+    background: require('@assets/images/green-background.png'),
+  },
+  {
+    name: 'pink',
+    background: require('@assets/images/pink-background.png'),
+  },
+  {
+    name: 'purple',
+    background: require('@assets/images/purple-background.png'),
+  },
+  {
+    name: 'red',
+    background: require('@assets/images/pink-background.png'),
+  },
+  {
+    name: 'white',
+    background: require('@assets/images/white-background.png'),
+  },
+  {
+    name: 'yellow',
+    background: require('@assets/images/yellow-background.png'),
+  },
+];
