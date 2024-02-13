4f7ab2553f8549e9908ed99f81423866d36c53cc
Schitzos <rizky.rachmawan25@gmail.com>
2024-02-13 11:19:20 +0700

    fix: add context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import Navigation from './navigation';
-import SplashScreen from 'react-native-splash-screen';
-import {QueryClientProvider} from '@tanstack/react-query';
-import {queryClientConfig} from './config/tanstack';
-
-const queryClient = queryClientConfig;
+import ContextProvider from './context';
 
 function App() {
-  useEffect(() => {
-    SplashScreen.hide();
-  }, []);
-
   return (
-    <QueryClientProvider client={queryClient}>
+    <ContextProvider>
       <Navigation />
-    </QueryClientProvider>
+    </ContextProvider>
   );
 }
 
