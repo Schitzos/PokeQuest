9db16a7107d63e648d890a4e673e0bd7cab28bfe
Schitzos <rizky.rachmawan25@gmail.com>
2024-03-18 03:32:50 +0700

    fix: preload navigation

--- src/screen/Preload/index.tsx
+++ src/screen/Preload/index.tsx
@@ -1,39 +1,28 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import {View} from 'react-native';
 import {PreloadScreenProps} from './type';
 import {useAsyncStorage} from '@react-native-async-storage/async-storage';
 
 export default function Preload({navigation}: PreloadScreenProps) {
   const {getItem} = useAsyncStorage('pokemon-data');
-  const [selectedPokemon, setSelectedPokemon] = useState<null | boolean>(null);
-  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     const checkSelected = async () => {
       try {
         const data = await getItem();
         if (data) {
-          setSelectedPokemon(true);
+          navigation.navigate('Dashboard');
         } else {
-          setSelectedPokemon(false);
+          navigation.navigate('Home');
         }
-        setLoaded(true);
       } catch (error) {
         console.error('Error check selected pokemon:', error);
       }
     };
 
     checkSelected();
-  }, [getItem]);
-
-  useEffect(() => {
-    if (loaded && selectedPokemon) {
-      navigation.navigate('Dashboard');
-    } else {
-      navigation.navigate('Home');
-    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [loaded]);
+  }, []);
 
   return <View />;
 }
