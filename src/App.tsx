683c0e38634593916bb5a7012f4ecc3fcc612147
Schitzos <rizky.rachmawan25@gmail.com>
2024-03-22 02:25:35 +0700

    fix: podfile lock and app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
+// App.js
+
 import React, {useEffect} from 'react';
-import {Alert} from 'react-native';
 import Navigation from './navigation';
 import ContextProvider from './context';
-import NetInfo from '@react-native-community/netinfo';
 import {LogBox} from 'react-native';
+import {setupNetInfoListener} from './utils/netInfo';
 
 function App() {
   useEffect(() => {
-    const unsubscribe = NetInfo.addEventListener(networkState => {
-      if (!networkState.isConnected) {
-        Alert.alert(
-          'No Internet Connection',
-          'Please check your internet connection and try again.',
-          [{text: 'OK', onPress: () => console.log('OK Pressed')}],
-        );
-      }
-    });
+    const unsubscribe = setupNetInfoListener();
 
     return () => {
       unsubscribe();
---
+++ src/utils/netInfo/index.ts
@@ -0,0 +1,16 @@
+// NetInfoListener.js
+
+import {Alert} from 'react-native';
+import NetInfo from '@react-native-community/netinfo';
+
+export function setupNetInfoListener() {
+  return NetInfo.addEventListener(networkState => {
+    if (!networkState.isConnected) {
+      Alert.alert(
+        'No Internet Connection',
+        'Please check your internet connection and try again.',
+        [{text: 'OK', onPress: () => console.log('OK Pressed')}],
+      );
+    }
+  });
+}
